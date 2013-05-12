1ef43eba09456142e65b22c87b4bc319b1de1d6c
doncc <896505009@qq.com>
2013-05-12 08:15:27 -0700

    1.更改main 2.添加新类

--- test_Helloword/src/dc/test_helloword/MainActivity.java
+++ test_Helloword/src/dc/test_helloword/MainActivity.java
@@ -12,13 +12,9 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new TestPage();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
 
     
 }
---
+++ test_Helloword/src/dc/test_helloword/TestPage.java
@@ -0,0 +1,8 @@
+package dc.test_helloword;
+
+public class TestPage {
+
+	public TestPage() {
+		System.out.println("this is test apge");
+	}
+}
